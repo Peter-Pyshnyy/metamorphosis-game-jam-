7bf1404da58493300de24413250e14f994ea7d6c
Peter-Pyshnyy <pyshnyy.peter@gmail.com>
2024-09-20 18:02:37 +0200

    added transformation

--- Camera3D.gd
+++ Camera3D.gd
@@ -6,14 +6,19 @@ var default_pitch := Vector3(deg_to_rad(-10), 0, 0)
 
 var is_zooming := false
 var is_zoomed := false
-var zoom_speed := 30.
-var close_fov := 20.0
-var close_pos := Vector3(0.35, 0, 3)
+var zoom_speed := 25.
+var close_fov := 27.
+var close_pos := Vector3(0.55, -0.75, 3)
 var close_pitch := Vector3(deg_to_rad(2.5), 0, 0)
 
 @onready var camera := $"."
 @onready var pitch := $".."
 @onready var twist := $"../.."
+
+@onready var body_mesh := $"../../../body_mesh"
+@onready var body_collision := $"../../../body_collision"
+@onready var gun_mesh := $"../../../gun_mesh"
+@onready var gun_collision := $"../../../gun_collision"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,14 +27,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#TODO: lock cam movement while zooming
 	if Input.is_action_pressed("zoom") and !is_zoomed:
 		is_zooming = true
 		camera.fov = lerp(camera.fov, close_fov, delta * zoom_speed)
 		camera.position = camera.position.lerp(close_pos, delta * zoom_speed)
 		pitch.rotation = pitch.rotation.lerp(close_pitch, delta * zoom_speed)
 		
-		if floor(camera.fov) == close_fov:
+		body_mesh.visible = false
+		body_collision.disabled = true
+		gun_mesh.visible = true
+		gun_collision.disabled = false
+		
+		print(camera.fov)
+		
+		if floor(camera.fov - 0.3) == close_fov:
 			camera.position = close_pos
 			pitch.rotation = close_pitch
 			is_zoomed = true
@@ -40,7 +51,10 @@ func _process(delta):
 		camera.position = camera.position.lerp(default_pos, delta * zoom_speed)
 		pitch.rotation = pitch.rotation.lerp(default_pitch, delta * zoom_speed)
 		
-		print(pitch.rotation)
+		body_mesh.visible = true
+		body_collision.disabled = false
+		gun_mesh.visible = false
+		gun_collision.disabled = true
 		
 		if ceil(camera.fov) == default_fov:
 			camera.position = default_pos
